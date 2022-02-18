13d8aede7e3687c19375b92c463a6c39782dd7ed
RJ <13499408+raehart@users.noreply.github.com>
2022-02-18 10:34:09 -0800

    Setup Axios & React Routing.  Setup using PostCard component for now.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import "./App.scss";
+import React from "react";
 import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
+
+import "./App.scss";
 import PostCard from "./components/PostCard";
+
+
 
 function App() {
   return (
     <div className="app">
       <Router>
         <Routes>
-          <Route path="/post-card" element={<PostCard/>}></Route>
+          {/* homePage */}
+          <Route path="/" element={<PostCard />}></Route>
+          <Route path="/post-card" element={<PostCard />}></Route>
+          {/* PostPage */}
+          {/* Author Page */}
         </Routes>
       </Router>
     </div>
--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -1,17 +1,35 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from "axios";
+
 import './PostCard.scss';
 
+
+const baseURL = "https://jsonplaceholder.typicode.com/posts/1";
+
 export default function PostCard() {
+  let [post, setPost] = useState("");
+  let [error, setError] = useState("");
+
+  React.useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      setPost(response.data);
+      console.log(`post: ${post}`);
+    }).catch(error => {
+      setError(error);
+      console.log(`error: ${error}`);
+    });
+  }, []);
+
+
   return (
     <div className="post-card">
       <div className="c-card">
         <div role="separator" className="c-card__item c-card__item--divider">
-          Header
+          <h2>{post ?  post.title : error}</h2>
         </div>
         <div className="c-card__item">
           <p className="c-paragraph">
-            Lorem ipsum dolor sit amet, feugiat corpora ex eam. Inciderint
-            eloquentiam sea et.
+            {post.body && post.body}
           </p>
         </div>
       </div>
