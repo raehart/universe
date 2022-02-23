c7fc2ef8a1a589ea002d292dbef8ad48454ec867
RJ <13499408+raehart@users.noreply.github.com>
2022-02-22 17:42:50 -0800

    Added Context.  Moved Axios call to App & using Context to pass data.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
 
 import "./App.scss";
+import { PostsContext } from "./contexts/PostsContext";
 import PostCard from "./components/PostCard";
 import PostList from "./components/PostList";
 import CommentCard from "./components/CommentCard";
 
-
-
 function App() {
+  const baseURL = "https://jsonplaceholder.typicode.com/posts/";
+
+  let [posts, setPosts] = useState([]);
+  let [error, setError] = useState({});
+
+  React.useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      setPosts(response.data);
+      console.log(`posts: ${posts}`);
+    }).catch(err => {
+      setError(err);
+      console.log(`error: ${err.message}`);
+    });
+  }, []);
+
   return (
     <div className="app">
-      <Router>
-        <Routes>
-          {/* homePage */}
-          <Route path="/" element={<PostList />}></Route>
-          <Route path="/post-card" element={<PostCard />}></Route>
-          <Route path="/comment" element={<CommentCard />}></Route>
-          {/* PostPage */}
-          {/* Author Page */}
-        </Routes>
-      </Router>
+      <PostsContext.Provider value={{posts, setPosts, error, setError}}>
+        <Router>
+          <Routes>
+            {/* homePage */}
+            <Route path="/" element={<PostList />}></Route>
+            {/* post/id */}
+            <Route path="/post/:id" element={<PostCard />}></Route>
+            <Route path="/comment" element={<CommentCard />}></Route>
+            {/* PostPage */}
+            {/* Author Page */}
+          </Routes>
+        </Router>
+      </PostsContext.Provider>
     </div>
   );
 }
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,33 +1,16 @@
-import React, {useState} from 'react';
-import axios from "axios";
+import React, { useContext } from "react";
+import { PostsContext } from "../contexts/PostsContext";
 
 import PostCard from "./PostCard";
 
-const baseURL = "https://jsonplaceholder.typicode.com/posts/";
-
 export default function PostList() {
-
-  let [posts, setPosts] = useState([]);
-  let [error, setError] = useState({});
-
-  React.useEffect(() => {
-    axios.get(baseURL).then((response) => {
-      setPosts(response.data);
-      console.log(`posts: ${posts}`);
-    }).catch(err => {
-      setError(err);
-      console.log(`error: ${err.message}`);
-    });
-  }, []);
-  
+  const {posts, error}  = useContext(PostsContext)
   return (
-
-      <div className="post-list">
-        {error && <div className="post-list-error">{error.message}</div>}
-        {posts.map((post, index) => {
-          return <PostCard key={index} post={post}/>
-        })}
-
-      </div>
+    <div className="post-list">
+      {error && <div className="post-list-error">{error.message}</div>}
+      {posts.map((post, index) => {
+        return <PostCard key={index} post={post} />;
+      })}
+    </div>
   );
 }
